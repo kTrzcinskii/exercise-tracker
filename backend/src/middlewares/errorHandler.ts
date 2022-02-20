dc11dfcdfeb5b28d9d1f53ea35b5ea0c647ff812
Kacper Trzciński <siema311@gmail.com>
2022-02-20 12:16:20 +0100

    added errorHandler middleware

---
+++ backend/src/middlewares/errorHandler.ts
@@ -0,0 +1,14 @@
+import { ErrorRequestHandler } from "express";
+import { StatusCodes } from "http-status-codes";
+import CustomError from "../errors/CustomError";
+
+const errorHandler: ErrorRequestHandler = (error: CustomError, _, res, __) => {
+  let CustomError = {
+    statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: error.message || "Something went wrong, try again later",
+  };
+
+  res.status(CustomError.statusCode).json({ CustomError: error.message });
+};
+
+export default errorHandler;
