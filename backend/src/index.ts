a9aac56d84b2be08d1fbf3c2f68665b6457bbdb5
Kacper Trzciński <siema311@gmail.com>
2022-02-20 12:16:42 +0100

    use middlewares in main

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,26 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import "express-async-errors";
+import notFound from "./middlewares/notFound";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 app.use(cors());
+
 app.use(express.static("public"));
 
 app.get("/", function (_: Request, res: Response) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("Your app is listening on port " + listener.address());
+//handling errors
+app.use(errorHandler);
+
+//last middleware
+app.use(notFound);
+
+const port = process.env.PORT || 5000;
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Your app is listening on port " + port);
 });
