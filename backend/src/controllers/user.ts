e9ecaf9b116ee75d4dbec6bd19b13b1801479230
Kacper Trzciński <siema311@gmail.com>
2022-02-22 18:01:26 +0100

    finish createNewExercise + setup basic getUserLogs

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import CustomError from "../errors/CustomError";
 import User from "../models/User";
+import Exercise from "../models/Exercise";
 
 export const getAllUsers = async (_: Request, res: Response) => {
   const users = await User.find({});
@@ -27,9 +28,69 @@ export const createNewUser = async (req: Request, res: Response) => {
 };
 
 export const createNewExercise = async (req: Request, res: Response) => {
-  res.send("create new exercise");
+  const userId = req.params._id;
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new CustomError(
+      "There is no user with provided id.",
+      StatusCodes.NOT_FOUND
+    );
+  }
+
+  const body = req.body;
+
+  if (!body.duration) {
+    throw new CustomError(
+      "You must provide duration time",
+      StatusCodes.BAD_REQUEST
+    );
+  }
+
+  if (!body.description) {
+    throw new CustomError(
+      "You must provide description",
+      StatusCodes.BAD_REQUEST
+    );
+  }
+
+  body.username = user.username;
+  body.userId = userId;
+
+  const exercise = await Exercise.create({ ...body });
+
+  res.status(StatusCodes.CREATED).json({
+    _id: exercise.userId,
+    username: user.username,
+    date: exercise.date.toDateString(),
+    duration: exercise.duration,
+    description: exercise.description,
+  });
 };
 
 export const getUserLogs = async (req: Request, res: Response) => {
-  res.send("get user logs");
+  const userId = req.params._id;
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new CustomError(
+      "There is no user with provided id.",
+      StatusCodes.NOT_FOUND
+    );
+  }
+
+  const exercises = await Exercise.find({ userId });
+
+  const log: any[] = [];
+
+  exercises.forEach((excercise) => {
+    const { duration, description, date } = excercise;
+    log.push({ description, duration, date: date.toDateString() });
+  });
+
+  const count = log.length;
+
+  res
+    .status(StatusCodes.OK)
+    .json({ _id: user._id, username: user.username, count, log });
 };
