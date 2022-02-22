683127b17833b4189bdc21425d1cc01141080d43
Kacper Trzciński <siema311@gmail.com>
2022-02-22 16:08:58 +0100

    added getAllUsers and createUser functionallity

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import CustomError from "../errors/CustomError";
+import User from "../models/User";
 
-export const getAllUsers = async (req: Request, res: Response) => {
-  res.send("get all the users");
+export const getAllUsers = async (_: Request, res: Response) => {
+  const users = await User.find({});
+
+  res.status(StatusCodes.OK).json(users);
 };
 
 export const createNewUser = async (req: Request, res: Response) => {
-  res.send("create new user");
+  const { username } = req.body;
+
+  if (!username) {
+    throw new CustomError(
+      "You must provide username to  create user",
+      StatusCodes.BAD_REQUEST
+    );
+  }
+
+  const user = await User.create({ username });
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ username: user.username, _id: user._id });
 };
 
 export const createNewExercise = async (req: Request, res: Response) => {
