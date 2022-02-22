08f3c6a67bf5b50ee2342c6cf0eaa1eaa1a22809
Kacper Trzciński <siema311@gmail.com>
2022-02-22 16:17:23 +0100

    added handling error with wrong id format

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -8,7 +8,13 @@ const errorHandler: ErrorRequestHandler = (error: CustomError, _, res, __) => {
     message: error.message || "Something went wrong, try again later",
   };
 
-  res.status(CustomError.statusCode).json({ Error: error.message });
+  if (error.message.includes("Cast to ObjectId failed")) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ Error: "Provided id has wrong format" });
+  }
+
+  return res.status(CustomError.statusCode).json({ Error: error.message });
 };
 
 export default errorHandler;
