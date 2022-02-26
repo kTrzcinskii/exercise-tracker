c3ab33b6a7e6362666243c3f015031c9c681cdaf
Kacper Trzciński <siema311@gmail.com>
2022-02-26 17:31:38 +0100

    remove bg-color

--- frontend/components/Home/HomeWrapper.tsx
+++ frontend/components/Home/HomeWrapper.tsx
@@ -2,7 +2,7 @@ interface HomeWrapperProps {}
 
 const HomeWrapper: React.FC<HomeWrapperProps> = ({ children }) => {
   return (
-    <div className='h-screen w-screen flex justify-center items-center bg-slate-200'>
+    <div className='h-screen w-screen flex justify-center items-center'>
       {children}
     </div>
   );
