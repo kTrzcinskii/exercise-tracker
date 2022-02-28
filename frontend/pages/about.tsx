fb0e79d3f88b82c431674b1971184511ccb077e6
Kacper Trzciński <siema311@gmail.com>
2022-02-28 16:49:02 +0100

    add content to about page

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
-import { route } from "next/dist/server/router";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import HiddenText from "../components/About/HiddenText";
+import MyLink from "../components/About/MyLink";
+import Technology from "../components/About/Technology";
 import Button from "../components/Button";
 
 interface AboutProps {}
@@ -16,77 +18,88 @@ const About: NextPage<AboutProps> = ({}) => {
       </h1>
       <div>
         <HiddenText heading='What is this site for?'>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia
-            voluptas animi ratione dolorem eligendi id aperiam doloremque
-            suscipit sint veritatis eveniet ea repellat saepe itaque et, eius
-            nam rerum. Nesciunt numquam pariatur et aut culpa repellat
-            cupiditate illo placeat, reiciendis dolorem, veniam eligendi rerum
-            obcaecati facilis expedita officiis, similique error. Vero eaque ut
-            voluptatum corrupti, illum aperiam sapiente earum. At corrupti,
-            magni aliquid repellendus, numquam beatae autem iste ipsum molestiae
-            adipisci enim officia voluptas ducimus. Possimus doloremque
-            voluptate praesentium alias, cupiditate iste exercitationem hic
-            ratione quod maxime tempore assumenda dolor vero quaerat nam
-            quisquam odio at impedit quibusdam culpa saepe.
-          </p>
+          <div>
+            <p>
+              Main goal of{" "}
+              <Link href='/' passHref>
+                <a className='text-blue-800 font-semibold hover:underline'>
+                  Exercise Tracker
+                </a>
+              </Link>{" "}
+              is to provide our users with convenient and reliable way of
+              keeping track of your activities. It does not have professional
+              analyses and is mostly focused on basic usage, such as checking
+              the duration of your activity, date and your custom describtion.
+              It is going to help you stay more organized and keep up with
+              fulfilling your goals.
+            </p>
+          </div>
         </HiddenText>
         <HiddenText heading='Why there is no authorization?'>
           <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
-            minus. Laudantium aut ea, quibusdam unde dolore tempore recusandae
-            odio doloribus?
+            <p>
+              Even though authorization is one of the most important components
+              on such a site, I cannot use it here because of very simple
+              reason. This site is part of{" "}
+              <Link
+                href='https://www.freecodecamp.org/learn/back-end-development-and-apis/#mongodb-and-mongoose'
+                passHref
+              >
+                <a
+                  className='text-blue-800 font-semibold hover:underline'
+                  target='_blank'
+                >
+                  freeCodeCamp course on backend technologies
+                </a>
+              </Link>{" "}
+              and in order to pass all the requirements there must be no
+              authorization on this site.
+            </p>
           </div>
         </HiddenText>
         <HiddenText heading='Reasons behind creating exercise tracker this way'>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
-            beatae provident rerum earum quibusdam, voluptate numquam ea quo
-            autem impedit perspiciatis sequi explicabo, voluptates sunt!
-            Repellendus sequi praesentium iste tempora? Repellat obcaecati
-            accusantium qui nisi unde impedit consequatur accusamus omnis, quasi
-            minus, iure, debitis doloremque quibusdam nam earum cum similique.
-          </p>
+          <div>
+            <p>
+              This project is one of the five final projects on freeCodeCamp
+              Backend Course. You can see more about it{" "}
+              <Link
+                href='https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/exercise-tracker'
+                passHref
+              >
+                <a
+                  className='text-blue-800 font-semibold hover:underline'
+                  target='_blank'
+                >
+                  here
+                </a>
+              </Link>
+            </p>
+            <p>
+              However, I&apos;ve decided I want to build a fullstack app so that
+              I can show a variety of skills I got from this and other courses
+              I&apos;ve taken on freeCodeCamp site.
+            </p>
+            <p className='text-gray-900 font-semibold mt-4 text-center text-lg lg:text-xl'>
+              Technologies I&apos;ve used while creating this project
+            </p>
+            <Technology />
+            <p className='italic'>Everything was written in TypeScript.</p>
+          </div>
         </HiddenText>
         <HiddenText heading='Created by'>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim minus
-            quos at voluptatibus ipsam facere temporibus qui ab similique
-            pariatur reiciendis rerum expedita suscipit est voluptate maiores
-            dolorem accusamus dolores nihil, laborum nemo commodi mollitia.
-            Quibusdam asperiores amet accusantium! Quos atque ea, laudantium
-            enim nam amet ab. Repudiandae quae neque itaque accusamus quibusdam
-            fuga debitis tenetur odio laborum nam. Mollitia eveniet sint veniam
-            similique ducimus, corporis harum itaque porro ut eos aspernatur,
-            error omnis amet repellendus labore ex nihil quaerat debitis
-            consequatur. Quo odio, consequuntur eveniet neque ipsum deserunt
-            incidunt, eaque velit nostrum dignissimos eos. Vitae vel unde odio
-            blanditiis repellendus debitis provident omnis eligendi expedita
-            atque, molestias, ab accusantium. Fugiat sunt impedit natus quaerat
-            ipsum dignissimos quibusdam aspernatur accusamus architecto,
-            sapiente illo alias enim obcaecati praesentium maiores, sit
-            cupiditate, soluta quam odit quo mollitia animi dolore autem. Nobis
-            mollitia blanditiis, sequi eaque, in maxime laborum perferendis,
-            ducimus illo aperiam consectetur iusto quam aspernatur placeat quo!
-            Maiores, illum deserunt, mollitia velit ratione praesentium rerum
-            ullam cupiditate suscipit reiciendis ducimus explicabo impedit amet
-            architecto quos delectus! Sed, modi ea iusto tempore error
-            reprehenderit eaque, adipisci earum repudiandae sint sit sapiente
-            nemo consequatur aut exercitationem? Soluta deleniti dolorem
-            exercitationem nihil provident omnis nisi hic dignissimos
-            consequuntur explicabo ex unde odit distinctio et id suscipit in est
-            numquam quae, rerum repudiandae facilis voluptas. Hic eveniet dolore
-            odio nihil eaque id, maiores recusandae ad, saepe accusantium
-            inventore vitae consectetur repellat neque esse et doloribus.
-            Voluptas, reiciendis qui optio temporibus ut aliquam iste excepturi,
-            voluptatibus molestiae, similique dolorem a? Praesentium repudiandae
-            voluptate commodi assumenda, neque eaque nam veritatis repellat
-            quidem labore ipsa ut dolorum obcaecati officiis harum laudantium at
-            saepe minus culpa! Iusto fugiat sapiente asperiores, similique illum
-            facilis. Explicabo esse aspernatur accusamus ex eos, dolores
-            perferendis iste eaque dolorem enim reiciendis velit asperiores
-            tempore?
-          </p>
+          <div>
+            <p className='font-semibold'>
+              This site was created by Kacper Trzciński
+            </p>
+            <p className='mt-3 font-semibold text-blue-800'>Useful links:</p>
+            <ul className='space-y-2 mt-2'>
+              <MyLink href='https://github.com/kTrzcinskii'>
+                GitHub Profile
+              </MyLink>
+              <MyLink href='/'>Project Repository</MyLink>
+              <MyLink href='/'>Backend</MyLink>
+            </ul>
+          </div>
         </HiddenText>
       </div>
       <div className='flex justify-center mt-6 lg:mt-10'>
